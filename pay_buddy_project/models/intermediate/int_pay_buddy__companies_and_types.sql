8d9341002346d08e850218b01fca403687f815ae
alexandredantec <alexandredantec@bodyguard.ai>
2023-08-07 17:07:57 +0200

    add intermediate tables

---
+++ pay_buddy_project/models/intermediate/int_pay_buddy__companies_and_types.sql
@@ -0,0 +1,24 @@
+-- int_pay_buddy__companies_and_types.sql
+
+WITH companies AS (
+  SELECT *
+  FROM {{ref('stg_pay_buddy__companies')}}
+)
+
+, company_types AS (
+  SELECT *
+  FROM {{ref('stg_pay_buddy__company_type')}} 
+)
+
+, companies_and_types AS (
+  SELECT
+  c.company_id,
+  c.company_name,
+  c.company_country,
+  c.company_reference,
+  ct.company_type
+  FROM companies AS c 
+  INNER JOIN company_types AS ct ON ct.company_type_id = c.company_type_id
+)
+
+SELECT * FROM companies_and_types
---
+++ pay_buddy_project/models/intermediate/int_pay_buddy__payment_plan_decisions.sql
@@ -0,0 +1,35 @@
+-- int_pay_buddy__payment_plan_decisions.sql
+
+WITH payment_plan_templates AS (
+
+SELECT * 
+FROM {{ref('stg_pay_buddy__payment_plan_templates')}}
+)
+
+, payment_plans AS (
+
+SELECT *
+FROM {{ref('stg_pay_buddy__payment_plans')}}
+)
+
+, decisions AS (
+
+SELECT *
+FROM {{ref('stg_pay_buddy__decisions')}}
+)
+
+, payment_plan_decisions AS (
+SELECT 
+p.payment_plan_id,
+p.order_id,
+pp.agreement_id,
+d.decision_type,
+d.decision_reason,
+pp.term_in_days
+FROM payment_plans AS p
+LEFT JOIN decisions AS d ON d.decision_id = p.decision_id
+LEFT JOIN payment_plan_templates AS pp ON pp.payment_plan_template_id = p.payment_plan_template_id
+
+)
+
+SELECT * FROM payment_plan_decisions
