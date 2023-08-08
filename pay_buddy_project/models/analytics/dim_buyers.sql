7fc745957f6e27b1a1dc9417f3c81d5f5095b633
alexandredantec <alexandredantec@bodyguard.ai>
2023-08-09 00:17:50 +0200

    add merchant and buyer dimensions

---
+++ pay_buddy_project/models/analytics/dim_buyers.sql
@@ -0,0 +1,18 @@
+-- dim_buyers.sql
+
+WITH buyer_companies AS (
+    SELECT *
+    FROM {{ref('int_pay_buddy__companies_and_types')}}
+    WHERE company_type = 'Buyer'
+)
+
+, buyers AS (
+    SELECT
+    company_id AS buyer_id,
+    company_name,
+    company_country,
+    company_reference,
+    FROM buyer_companies
+)
+
+SELECT * FROM buyers
---
+++ pay_buddy_project/models/analytics/dim_merchants.sql
@@ -0,0 +1,18 @@
+-- dim_merchants.sql
+
+WITH merchant_companies AS (
+  SELECT *
+  FROM {{ref('int_pay_buddy__companies_and_types')}}
+  WHERE company_type = 'Merchant'
+)
+
+, merchants AS (
+  SELECT
+  company_id AS merchant_id,
+  company_name,
+  company_country,
+  company_reference,
+  FROM merchant_companies AS c 
+)
+
+SELECT * FROM merchants
