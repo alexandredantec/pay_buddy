73c84322dfd292921446b727b62712d593b280cf
alexandredantec <alexandredantec@bodyguard.ai>
2023-08-10 10:24:46 +0200

    enable documentation everywhere

--- pay_buddy_project/models/analytics/buyers.sql
+++ pay_buddy_project/models/analytics/buyers.sql
@@ -3,7 +3,7 @@
 WITH buyer_companies AS (
     SELECT *
     FROM {{ref('int_pay_buddy__companies_and_types')}}
-    WHERE company_type = 'Buyer'
+    
 )
 
 , buyers AS (
@@ -13,6 +13,7 @@ WITH buyer_companies AS (
     company_country,
     company_reference,
     FROM buyer_companies
+    WHERE company_type = 'Buyer'
 )
 
 SELECT * FROM buyers
--- pay_buddy_project/models/analytics/merchants.sql
+++ pay_buddy_project/models/analytics/merchants.sql
@@ -3,7 +3,7 @@
 WITH merchant_companies AS (
   SELECT *
   FROM {{ref('int_pay_buddy__companies_and_types')}}
-  WHERE company_type = 'Merchant'
+  
 )
 
 , merchants AS (
@@ -13,6 +13,7 @@ WITH merchant_companies AS (
   company_country,
   company_reference,
   FROM merchant_companies AS c 
+  WHERE company_type = 'Merchant'
 )
 
 SELECT * FROM merchants
